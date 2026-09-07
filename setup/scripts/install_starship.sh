452c8de6e6297eb12c63da1cdf7abc0a793cc082
Viktors Vipolzovs <viktor.vipolzov@gmail.com>
2026-09-07 21:56:18 +0300

    Fix Starship release asset name

--- setup/scripts/install_starship.sh
+++ setup/scripts/install_starship.sh
@@ -4,9 +4,20 @@ set -e
 echo "Installing Starship prompt..."
 
 # Download and install Starship
-STARSHIP_VERSION=$(curl -s https://github.com/starship/starship/releases/latest | grep '"tag_name"' | sed 's/.*"\([^"]*\)".*/\1/')
+STARSHIP_VERSION=$(curl -fsSL https://api.github.com/repos/starship/starship/releases/latest | grep '"tag_name"' | sed 's/.*"\([^"]*\)".*/\1/')
 
-curl -L "https://github.com/starship/starship/releases/download/${STARSHIP_VERSION}/starship-linux-amd64.tar.gz" -o /tmp/starship.tar.gz
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) STARSHIP_ARCH="x86_64" ;;
+    aarch64) STARSHIP_ARCH="aarch64" ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+ASSET="starship-${STARSHIP_ARCH}-unknown-linux-gnu.tar.gz"
+curl -fL "https://github.com/starship/starship/releases/download/${STARSHIP_VERSION}/${ASSET}" -o /tmp/starship.tar.gz
 tar -xzf /tmp/starship.tar.gz -C /usr/local/bin
 chmod +x /usr/local/bin/starship
 rm -f /tmp/starship.tar.gz
